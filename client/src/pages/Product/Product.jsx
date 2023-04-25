152c762b4fbb909cc3a7f03c9fd1a2cff4dd3ceb
HystERICKal <nyoroerick12@gmail.com>
2023-04-25 19:21:20 +0300

    Finished Fetching all Products From Strapi Backend - Though It's Still A Bit Buddy (Will Fix At A Later Stage, Probably After Adding Stripe Functionality)

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -4,71 +4,101 @@ import { useState } from "react";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(1);
 
-  const images = [
-    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-  ];
+  // const images = [
+  //   "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  //   "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  // ];
+
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
-        </div>
-        <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
-        </div>
-      </div>
-      <div className="right">
-        <h1>Title</h1>
-        <span className="price">KSH200</span>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error totam
-          libero modi earum vitae placeat eaque porro asperiores iste recusandae
-          nostrum, dolore at repellat corrupti ipsam quibusdam optio cum
-          voluptate!
-        </p>
-        <div className="quantity">
-          <button
-            onClick={() =>
-              quantity > 1 ? setQuantity(quantity - 1) : setQuantity(1)
-            }
-          >
-            -
-          </button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
-        <button className="add">
-          <AddShoppingCartIcon /> ADD TO CART
-        </button>
-        <div className="links">
-          <div className="item">
-            <FavoriteBorderIcon /> ADD TO WISH LIST
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              {/* <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} /> */}
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              {/* <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} /> */}
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className="mainImg">
+              {/* <img src={images[selectedImg]} alt="" /> */}
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
           </div>
-          <div className="item">
-            <BalanceIcon /> ADD TO COMPARE
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className="price">KSH{data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+            <div className="quantity">
+              <button
+                onClick={() =>
+                  quantity > 1 ? setQuantity(quantity - 1) : setQuantity(1)
+                }
+              >
+                -
+              </button>
+              {quantity}
+              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
+            <button className="add">
+              <AddShoppingCartIcon /> ADD TO CART
+            </button>
+            <div className="links">
+              <div className="item">
+                <FavoriteBorderIcon /> ADD TO WISH LIST
+              </div>
+              <div className="item">
+                <BalanceIcon /> ADD TO COMPARE
+              </div>
+            </div>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
           </div>
-        </div>
-        <div className="info">
-          <span>Vendor: Polo</span>
-          <span>Product Type: T-Shirt</span>
-          <span>Tag: T-Shirt, Women, Top</span>
-        </div>
-        <hr />
-        <div className="info">
-          <span>DESCRIPTION</span>
-          <hr />
-          <span>ADDITIONAL INFORMATION</span>
-          <hr />
-          <span>FAQ</span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ client/src/redux/cartReducer.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    products: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      //action like 'clicking AddToCart button'
+      // First check if the item has been added to cart before, if it has,
+      //     then don't add it again, instead, just increase it's quantity
+
+      const item = state.products.find((item) => item.id === action.payload.id);
+
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+
+      //In short, redux-toolkit allows pushing items directly like this and
+      //    changing object property like shown below since it uses a library called immutable js
+      if (item) {
+        //Increment the item that already exists in cart
+        item.quantity += action.payload.quantity; //change object property
+      } else {
+        state.push(action.payload); //push item to cart
+      }
+
+      //   state.value += 1;
+    },
+    removeItem: (state, action) => {
+      state.products = state.products.filter(
+        (item) => item.id !== action.payload
+      ); //delete item
+
+      //   state.value -= 1;
+    },
+    resetCart: (state) => {
+      state.products = [];
+      //   state.value += action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToCart, removeItem, resetCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
